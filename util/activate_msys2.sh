1a90f5f1e94adf818f9446c4d0b4c0c864407155
Pierre Constantineau <jpconstantineau@gmail.com>
2018-04-13 18:03:06 -0700

    adding nordic tools

--- util/activate_msys2.sh
+++ util/activate_msys2.sh
@@ -4,6 +4,9 @@ function export_variables {
     local util_dir=~/util
     export PATH=$PATH:$util_dir
     export PATH=$PATH:$util_dir/gcc-arm-none-eabi/bin
+    export PATH=$PATH:/C/Program Files (x86)/Nordic Semiconductor/nrf5x/bin
+
+    
 }
 
 export_variables
