56c2de8a40dc780122585a65fc495d6784374fe0
gh-actions <actions@gihub.com>
2020-11-23 04:06:09 +0000

    Deploy website - based on d94c90ece3deac68810f4e66d55099d54f6c9bea

---
+++ d0766ab8.3bc04bca.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[16],{84:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/blog/welcome"}]}')}}]);
