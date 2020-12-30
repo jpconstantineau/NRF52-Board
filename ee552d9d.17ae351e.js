71c61b0bdb9f6e1952abace977e12742447a356d
gh-actions <actions@gihub.com>
2020-12-30 18:25:02 +0000

    Deploy website - based on 008885e514a2c8cd81ff8ab3ca8844e20a0e6383

---
+++ ee552d9d.17ae351e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[22],{88:function(o){o.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"blue-micro","name":"bluemicro","count":1,"permalink":"/blog/tags/blue-micro"}')}}]);
