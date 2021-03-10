ae5ccc24df694f634ce9773e10dfbe3f9cd33910
Pierre <jpconstantineau@gmail.com>
2021-03-09 22:49:37 -0600

    updated overview

--- documentation/sidebars.js
+++ documentation/sidebars.js
@@ -3,7 +3,7 @@ module.exports = {
     'Getting Started': ['gettingstarted'],
     'nRF52832 Boards': ['bluemicro_v1', 'bluemicro_v2_0','bluemicro_v2_1'],
     'nRF52840 Boards': ['bluemicro840_v1','bluemacro840_v1'],
-    'Macropads': ['macropad/bluehand','macropad/4x4macropad'],
+    'Macropads': ['macropad/bluehand','macropad/4x4macropad','overview'],
     'Programmers': ['programmer/programmer_overview','programmer/programmer_nrf52bmp'],
   },
 };
